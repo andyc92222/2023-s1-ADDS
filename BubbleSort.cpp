8942a5b6ab2963d0e39dbb48d2178f1f5d465600
andyc92222 <126429483+andyc92222@users.noreply.github.com>
2023-09-04 17:27:28 +0930

    4

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,8 +1,8 @@
 #include "BubbleSort.h"
 
 std::vector<int> BubbleSort::sort(std::vector<int> list) {
-    for (int i=0; i<list.size()-1; i++){
-        for (int j=0;j<list.size()-i-1;j++){
+    for (unsigned long int i=0; i<list.size()-1; i++){
+        for (unsigned long int j=0;j<list.size()-i-1;j++){
             if (list[j]>list[j+1]){
                 std::swap(list[j],list[j+1]);
             }
